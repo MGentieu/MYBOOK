fbcde4f0bb8c75f964348602f2639a36f847aa8c
MGentieu <mgentieu02@gmail.com>
2023-12-26 17:40:38 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Mybook</title>
+
+</head> 
+
+<body>
+	<div style="height: 100px;
+		background-color: #3c5a99;
+		color: #d9dfeb;
+		font-size: 40px;
+		">
+		Mybook Signup
+	</div>
+</body>
+
+
+
+</html>
